1700b18af22aee162587f3b3283e9be04664461b
Dmitry Glazunov <dima.glazun73@gmail.com>
2022-11-04 17:28:59 +0300

    Made a nice view of code

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import prometheus_client as pr
 import os.path
+import subprocess
 
 EXPORTER_PORT = 10101
-PATHS_TO_DATABASES = {"First db" : "C:\\REDSOFT\\TestDB.fdb"}
-PATHS_TO_UTILITIES = {"gstat" : "C:\\REDSOFT\\gstat.exe"}
+PATHS_TO_DATABASES = {"First db": "C:\\REDSOFT\\TestDB.fdb"}
+PATHS_TO_UTILITIES = {"gstat": "C:\\REDSOFT\\gstat.exe"}
 
 gauge_database_size = pr.Gauge("RedDatabase_db_size", "The amount of database bytes which are taken to store data.")
 gauge_diff_oldt_nt_size = pr.Gauge("RedDatabase_diff_oldt_nt", "The difference between Oldest transaction number and Next transaction numbers.")
+
 
 def scrape_db_size(path_to_database):
     if os.path.exists(path_to_database):
         db_size_in_bytes = os.path.getsize(path_to_database)
         gauge_database_size.set(db_size_in_bytes)
+
 
 def scrape_transactions(path_to_gstat, path_to_database):
     out = subprocess.run([path_to_gstat, '-h', path_to_database], capture_output=True).stdout
@@ -27,7 +30,6 @@ def scrape_transactions(path_to_gstat, path_to_database):
     difference_OLDT_NT = int(p["Next transaction"]) - int(p["Oldest transaction"])
     gauge_diff_oldt_nt_size.set(difference_OLDT_NT)
     
-
 
 if __name__ == "__main__":
     pr.start_http_server(EXPORTER_PORT)
