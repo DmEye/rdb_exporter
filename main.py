c257202e7829545b1618766a0bf48b6e7c7a8077
Dmitry Glazunov <dima.glazun73@gmail.com>
2022-11-08 11:36:46 +0300

    The ability to run server has been added

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import prometheus_client as pr
 import os.path
 import subprocess
+from http.server import BaseHTTPRequestHandler
+from http.server import HTTPServer
+
+def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
 
 EXPORTER_PORT = 10101
 PATHS_TO_DATABASES = {"First db": "C:\\REDSOFT\\TestDB.fdb"}
