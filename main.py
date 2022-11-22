593ff6fb6bd3b3f7a1a62f9e3e7f85a82800baa5
Dmitry Glazunov <dima.glazun73@gmail.com>
2022-11-22 23:19:05 +0300

    Exporter is based on built-in http python module now. Some metrics have been added. An exporter configurate json file is required to work now.

--- main.py
+++ main.py
@@ -1,48 +1,74 @@
-import prometheus_client as pr
 import os.path
 import subprocess
 from http.server import BaseHTTPRequestHandler
 from http.server import HTTPServer
+import json
 
-def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-    server_address = ('', 8000)
+CONFIGURE = {}
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/metrics":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(self.scrape_db_size(CONFIGURE["databases"]["test_db"]).encode())
+            self.wfile.write(self.scrape_transactions(CONFIGURE["utilities"]["gstat"], CONFIGURE["databases"]["test_db"]).encode())
+            self.wfile.write(self.scrape_active_users(CONFIGURE["utilities"]["isql"], CONFIGURE["databases"]["test_db"], CONFIGURE["login"], CONFIGURE["password"]).encode())
+        else:
+            self.send_response(404)
+
+    def scrape_db_size(self, path_to_database) -> str:
+        db_size_in_bytes = 0
+        if os.path.exists(path_to_database):
+            db_size_in_bytes = os.path.getsize(path_to_database)
+        return "reddatabase_db_size{} " + str(db_size_in_bytes) + "\n"
+
+    def scrape_transactions(self, path_to_gstat, path_to_database) -> str:
+        out = subprocess.run([path_to_gstat, '-h', path_to_database], capture_output=True).stdout
+        out = [row.replace('\r', '') for row in str(out, encoding="UTF-8").split('\n')]
+        for i in range(out.count('')):
+            out.remove('')
+        out = [row[1:] for row in out]
+        p = {}
+        for row in out:
+            buffer = row.split('\t')
+            p[buffer[0]] = buffer[-1]
+        del out
+        difference_OLDT_NT = int(p["Next transaction"]) - int(p["Oldest transaction"])
+        return "RedDatabase_diff_oldt_nt{} " + str(difference_OLDT_NT) + "\n"
+
+    def scrape_active_users(self, path_to_isql, path_to_database, login, password):
+        with subprocess.Popen(path_to_isql, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                              shell=True) as isql:
+            out = isql.communicate(
+                f"CONNECT '{path_to_database}' USER '{login}' PASSWORD '{password}'; SHOW USERS; QUIT;".encode())
+        out = out[0].decode("UTF-8")  # get the string
+        # extracting data
+        out = out.split('\n')
+        del out[0]  # remove header
+        del out[-1]  # remove empty string
+
+        amount_of_active_users = 0
+        for record in out:
+            active = int(record.split()[0])
+            amount_of_active_users += active
+
+        return "RedDatabase_active_users{} " + str(amount_of_active_users) + "\n"
+
+
+def run(server_class=HTTPServer, handler_class=Handler):
+    server_address = ('', CONFIGURE["port"])
     httpd = server_class(server_address, handler_class)
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         httpd.server_close()
 
-EXPORTER_PORT = 10101
-PATHS_TO_DATABASES = {"First db": "C:\\REDSOFT\\TestDB.fdb"}
-PATHS_TO_UTILITIES = {"gstat": "C:\\REDSOFT\\gstat.exe"}
-
-gauge_database_size = pr.Gauge("RedDatabase_db_size", "The amount of database bytes which are taken to store data.")
-gauge_diff_oldt_nt_size = pr.Gauge("RedDatabase_diff_oldt_nt", "The difference between Oldest transaction number and Next transaction numbers.")
-
-
-def scrape_db_size(path_to_database):
-    if os.path.exists(path_to_database):
-        db_size_in_bytes = os.path.getsize(path_to_database)
-        gauge_database_size.set(db_size_in_bytes)
-
-
-def scrape_transactions(path_to_gstat, path_to_database):
-    out = subprocess.run([path_to_gstat, '-h', path_to_database], capture_output=True).stdout
-    out = [row.replace('\r', '') for row in str(out, encoding="UTF-8").split('\n')]
-    for i in range(out.count('')):
-        out.remove('')
-    out = [row[1:] for row in out]
-    p = {}
-    for row in out:
-        buffer = row.split('\t')
-        p[buffer[0]] = buffer[-1]
-    del out
-    difference_OLDT_NT = int(p["Next transaction"]) - int(p["Oldest transaction"])
-    gauge_diff_oldt_nt_size.set(difference_OLDT_NT)
-    
 
 if __name__ == "__main__":
-    pr.start_http_server(EXPORTER_PORT)
-    while True:
-        scrape_db_size(PATHS_TO_DATABASES["First db"])
-        scrape_transactions(PATHS_TO_UTILITIES["gstat"], PATHS_TO_DATABASES["First db"])
+    # Parsing
+    with open("./exporter_conf.json", "r") as file:
+        CONFIGURE = json.loads(file.readline().encode())
+    run()
+
